fc03db2b4096a072d705cd2e89f9a401a7fb430f
tajush <tajusabd@gmail.com>
2024-11-07 22:33:38 +0600

    loop-return

---
+++ assets/init-loop.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (typeof LoopOnstore !== 'undefined') {
+        LoopOnstore.init({
+            key: "6b3ad177d41f88485414f277a1d6972aa141ab76", 
+            attach: ".button, .button--primary, .button--full-width, .button--arrow"
+        });
+    }
+});
