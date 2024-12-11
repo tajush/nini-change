8a2ce9752d30907d9a7a2f3a7e34f63960b92217
tajush <tajusabd@gmail.com>
2024-12-11 11:23:21 +0600

    all

---
+++ assets/init-loop.js
@@ -0,0 +1,16 @@
+//    document.addEventListener('DOMContentLoaded', function() {
+//     const loop = LoopOnstore.init({ 
+//         key: "8f452ebf955050bef8696083f097fb48c97157cd", 
+//         attach: "#CartDrawer-Checkout, #exchange", 
+//     });
+//     console.log(LoopOnstore);
+//     console.log(LoopOnstore.isActive());
+//     console.log({ loop })
+    
+// });
+
+// LoopOnstore.init({ 
+//     key: "8f452ebf955050bef8696083f097fb48c97157cd", 
+//     attach: "#CartDrawer-Checkout, #exchange", 
+//     });   
+//     console.log(LoopOnstore.isActive());
